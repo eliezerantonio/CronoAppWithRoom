b9b6c27056d21c4c5165507b168b022b50ec4cf6
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-09-07 07:40:49 +0100

    feat:  cronos repository implementation

--- app/src/main/java/com/example/cronoapp_e_room/repository/CronosRepository.kt
+++ app/src/main/java/com/example/cronoapp_e_room/repository/CronosRepository.kt
@@ -1,5 +1,23 @@
 package com.example.cronoapp_e_room.repository
 
-class CronosRepository {
+import com.example.cronoapp_e_room.model.Cronos
+import com.example.cronoapp_e_room.room.CronosDatabaseDao
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.conflate
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CronosRepository @Inject constructor(private val cronosDatabaseDao: CronosDatabaseDao) {
+
+    suspend fun addCrono(crono: Cronos) = cronosDatabaseDao.insert(crono)
+    suspend fun updateCrono(crono: Cronos) {
+        cronosDatabaseDao.update(crono)
+    }
+    suspend fun deleteCrono(crono: Cronos) = cronosDatabaseDao.delete(crono)
+    fun getAllCronos(): Flow<List<Cronos>> = cronosDatabaseDao.getCronos().flowOn(Dispatchers.IO).conflate()
+    fun getCronoById(id: Long): Flow<Cronos> = cronosDatabaseDao.getCronosById(id).flowOn(Dispatchers.IO).conflate()
+
+
 
 }
